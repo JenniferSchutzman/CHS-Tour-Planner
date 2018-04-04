d0c20439c65c6c647d4a0bb81f30483c4cb52492
Jennifer Schutzman <jennifer.schutzman@gmail.com>
2018-04-04 11:56:16 -0400

    mapped history state for grid

--- app/src/pages/experience-type/History.js
+++ app/src/pages/experience-type/History.js
@@ -7,7 +7,7 @@ import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList'
 import Subheader from 'material-ui/List/ListSubheader'
 import IconButton from 'material-ui/IconButton'
 import InfoIcon from 'material-ui-icons/Info'
-import { map } from 'ramda'
+import { prop, find, compose } from 'ramda'
 import Button from 'material-ui/Button'
 import classNames from 'classnames'
 
@@ -31,17 +31,46 @@ const styles = theme => ({
 const History = props => {
   const { classes } = props
   console.log('inside History state', props)
-  const data = props.stateTracker
+  const data = compose(
+    prop('experienceTypes'),
+    find(x => x.name === 'History')
+  )(props.insideInterests)
+  console.log('data inside history', JSON.stringify(data))
+  const fakeData = [
+    { name: 'Museum', img: '../../public/white_Museum.jpg' },
+    { name: 'Carriage', img: '../../public/hisCharCarriageTour.jpg' },
+    { name: 'Boat', img: '../../public/sailing.jpeg' },
+    { name: 'Walking', img: '../../public/street.png' }
+  ]
   return (
-    <GridList cellHeight={180}>
-      <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-        <Subheader component="div">
-          Which type of experience do you prefer?{' '}
-        </Subheader>
-      </GridListTile>
-    </GridList>
+    <div>
+      <GridList cellHeight={180}>
+        <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+          <Subheader component="div">
+            Which type of experience do you prefer?{' '}
+          </Subheader>
+        </GridListTile>
+        {data.map(tile => (
+          <GridListTile>
+            <Link to={'/schedule'} style={{ textDecoration: 'none' }}>
+              <img src={tile.img} alt={tile.name} />
+            </Link>
+            <GridListTileBar title={tile.name} />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
   )
 }
+
+// {data.map(tile => (
+//   <GridListTile>
+//     <Link to={'/schedule'} style={{ textDecoration: 'none' }}>
+//       <img src={tile.img} alt={tile.name} />
+//     </Link>
+//     <GridListTileBar title={tile.name} />
+//   </GridListTile>
+// ))}
 
 function mapStateToProps(state) {
   return {
