d8c295e432faca368c4af7642046925fce946430
Jennifer Schutzman <jennifer.schutzman@gmail.com>
2018-07-13 11:58:07 -0400

    Updated databse server

--- api/lib/dal-helper.js
+++ api/lib/dal-helper.js
@@ -1,29 +1,31 @@
-require('dotenv').config()
-const PouchDB = require('pouchdb-core')
-PouchDB.plugin(require('pouchdb-adapter-http'))
-PouchDB.plugin(require('pouchdb-find'))
-const HTTPError = require('node-http-error')
-const { pluck } = require('ramda')
+require('dotenv').config();
+const PouchDB = require('pouchdb-core');
+PouchDB.plugin(require('pouchdb-adapter-http'));
+PouchDB.plugin(require('pouchdb-find'));
+const HTTPError = require('node-http-error');
+const { pluck } = require('ramda');
 
-const db = new PouchDB(`${process.env.COUCH_URL}${process.env.COUCH_DBNAME}`)
+const db = new PouchDB(
+	`${process.env.COUCHDB_URL}${process.env.COUCHDB_DATABASE}`
+);
 
 const allDocs = tours => {
-  return db.allDocs(tours).then(docs => pluck('doc', docs.rows))
-}
+	return db.allDocs(tours).then(docs => pluck('doc', docs.rows));
+};
 
-const getDoc = id => db.get(id)
-const addDoc = doc => db.put(doc)
+const getDoc = id => db.get(id);
+const addDoc = doc => db.put(doc);
 const updateDoc = doc => {
-  return db.put(doc)
-}
-const deleteDoc = id => db.get(id).then(doc => db.remove(doc))
+	return db.put(doc);
+};
+const deleteDoc = id => db.get(id).then(doc => db.remove(doc));
 
 const dalHelper = {
-  allDocs,
-  getDoc,
-  deleteDoc,
-  addDoc,
-  updateDoc
-}
+	allDocs,
+	getDoc,
+	deleteDoc,
+	addDoc,
+	updateDoc
+};
 
-module.exports = dalHelper
+module.exports = dalHelper;
--- api/server.js
+++ api/server.js
@@ -1,20 +1,20 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const PORT = process.env.PORT || 5000
-const tourRoute = require('./routes/tours')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+const tourRoute = require('./routes/tours');
 
-app.use(bodyParser.json())
-app.use(cors({ credentials: true }))
+app.use(bodyParser.json());
+app.use(cors({ credentials: true }));
 
 // app.use(cors({ credentials: true }))
 
 app.get('/', (req, res) => {
-  res.status(200).send('Welcome to book charleston tours')
-})
+	res.status(200).send('Welcome to book charleston tours');
+});
 
-tourRoute(app)
+tourRoute(app);
 
-app.listen(PORT, () => console.log('API UP on', PORT))
+app.listen(PORT, () => console.log('API UP on', PORT));
